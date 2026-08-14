3ddd7a74417286604f332bae7c1430bf1ef7ca61
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-14 17:13:48 +0200

    Adding Tailwind styling

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -4,15 +4,20 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div>
-        <header>
-            <nav>
-                <RouterLink to="">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-            <h1>Oliver Verdesseldonck</h1>
+    <div class="flex items-center flex-col">
+        <header class="w-full bg-cyan-500">
+            <div class="flex justify-between">
+                <img alt="logo" class="h-8"/>
+                <nav class="flex justify-center gap-2 bg-indigo-600">
+                    <RouterLink to="/" class="px-2">Home</RouterLink>
+                    <RouterLink to="/about" class="px-2">About</RouterLink>
+                </nav>
+            </div>
+            <div class="max-w-7xl mx-auto mt-4">
+                <h1 class="text-5xl font-bold">Oliver Verdesseldonck</h1>
+            </div>
         </header>
-        <main>
+        <main class="w-full flex-1 max-w-7xl mx-auto px-6 py-4">
             <slot />
         </main>
         <footer>
