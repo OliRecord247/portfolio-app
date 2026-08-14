6e8f0fb7b483a8a1b590d32d67c278d643f7f1b7
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-14 16:42:31 +0200

    Make route pages

---
+++ src/components/layouts/DefaultLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <div>
+        <header>
+            <nav>
+                <RouterLink to="">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+            <h1>Oliver Verdesseldonck</h1>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>
+            <p>&copy; 2026 - Fullstack developer</p>
+        </footer>
+    </div>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../components/views/HelloWorld.vue'
+import AboutView from '../components/views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
