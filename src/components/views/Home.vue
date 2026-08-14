a5f90d90f1342de06b6034ae1a54562f2e1e519f
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-14 19:27:58 +0200

    Quick setup using vue-query

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,3 +1,23 @@
+<script lang="ts" setup>
+import { useQuery } from '@tanstack/vue-query'
+
+const { isLoading, data } = useQuery({
+    queryKey: ['skills'],
+    queryFn: async () => {
+        const res = await fetch("http://localhost:5000/skills");
+        const data = await res.json();
+        return data;
+    }
+})
+</script>
+
 <template>
-    <p>About page</p>
+    <div v-if="isLoading">
+        <p>Loading...</p>
+    </div>
+    <div v-else>
+        <ul>
+            <li v-for="item in data" :key="item.id">{{ item.name }}</li>
+        </ul>
+    </div>
 </template>
