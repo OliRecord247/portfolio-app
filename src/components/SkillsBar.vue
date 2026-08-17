fb9371d68d0a182914d99ad50c2298a0c21caa1b
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-18 00:14:24 +0200

    Use skillbar with composable

---
+++ src/components/SkillsBar.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useSkills } from '../composables/useSkills'
+
+const {
+    data: skills,
+    isLoading,
+    isError
+} = useSkills();
+</script>
+
+<template>
+    <div v-if="isLoading || isError">
+        <p>{{(isError) ? "Error" : "Loading..."}}</p>
+    </div>
+    <ul class="flex gap-2 justify-center items-center">
+        <li 
+            v-for="skill in skills"
+            class="px-2 py-1 rounded-lg bg-accent text-textLight"
+        >{{skill.name}}</li>
+    </ul>
+</template>
