5a892aa65eef9d3b5dc3c5fc7716b1e9947c290e
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-18 00:12:39 +0200

    Build composable to fetch skills

---
+++ src/composables/useSkills.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/vue-query'
+
+type Skill = {
+    name: string,
+    level: number
+}
+
+async function fetchSkills(): Promise<Skill[]> {
+    const response = await fetch("http://localhost:5000/skills");
+    if (!response.ok) {
+        throw new Error('Failed to fetch skills');
+    }
+
+    return response.json();
+}
+
+export function useSkills() {
+    return useQuery({
+        queryKey: ['skills'],
+        queryFn: fetchSkills,
+        staleTime: 1000 * 60 * 5,
+    })
+}
