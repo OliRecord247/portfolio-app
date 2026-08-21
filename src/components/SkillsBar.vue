e9c602ea77ba9d88a10f3568a59b8adb50bdb473
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-08-21 11:20:32 +0200

    Organise skillbar with shoosing to show one element

--- src/components/SkillsBar.vue
+++ src/components/SkillsBar.vue
@@ -9,19 +9,19 @@ const {
 </script>
 
 <template>
-    <div v-if="isLoading || isError">
+    <div v-if="isLoading || isError" class="flex justify-center items-center">
         <p>{{(isError) ? "Error&hellip;" : "Loading&hellip;"}}</p>
     </div>
-    <ul class="flex gap-5 flex-wrap justify-end">
+    <ul v-else class="flex gap-5 flex-wrap justify-end">
         <li 
             v-for="skill in skills"
             class="px-2 py-1 rounded-lg flex gap-2 items-center bg-surface text-textLight"
             :title="skill.name"
         >
             <div class="px-2 py-1 rounded-lg bg-accent/80">
-                <img :src="`/images/${skill.image}`" :alt="`${skill.name}-logo`" class="h-15"/>
+                <img  v-if="skill.image" :src="`/images/${skill.image}`" :alt="`${skill.name}-logo`" class="h-15"/>
+                <p v-else>{{ skill.name }}</p>
             </div>
-            <span>{{ skill.name }}</span>
         </li>
     </ul>
 </template>
